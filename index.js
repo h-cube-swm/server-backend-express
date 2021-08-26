b505d75724bb80049a7eb585b75ba87490e460ac
SeungWookHan <hswook12@me.com>
2021-08-26 15:49:49 +0000

    Add Survey put method

--- index.js
+++ index.js
@@ -89,6 +89,7 @@ const connectDB = async () => {
       {
         useUnifiedTopology: true,
         useNewUrlParser: true,
+        useFindAndModify: false,
       }
     );
 
--- routes/survey.js
+++ routes/survey.js
@@ -1,22 +1,16 @@
 const express = require("express");
-const { getResponse } = require("../utils");
+const { getResponse: gr, getComment: gc } = require("../utils");
 const Survey = require("../models/survey");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.status(200).send(getResponse(req.survey, "Get Success"));
+  res.status(200).send(gr(req.survey, "Get Success"));
 });
 
-router.post("/", (req, res, next) => {
-  Survey.create(req.body)
-    .then((newSurvey) => {
-      res.status(200).send(getResponse(newSurvey, "Create Success"));
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send(getResponse(null, "Error"));
-    });
+router.put("/", async (req, res, next) => {
+  await Survey.updateOne(req.survey, req.body).exec();
+  res.status(200).send(gc("Update Success"));
 });
 
 module.exports = router;
