ace7c953f34de898ae93d475a38d5bd87cdc986a
SeungWookHan <hswook12@me.com>
2021-08-27 06:47:26 +0000

    Add Response router logic

--- routes/response.js
+++ routes/response.js
@@ -1,11 +1,31 @@
 const express = require("express");
-const { getResponse } = require("../utils");
+const { getResponse: gr, getComment: gc } = require("../utils");
 const Response = require("../models/response");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {});
+router.get("/", async (req, res) => {
+  try {
+    const responses = await Response.find({ surveyId: req.survey._id });
+    result = {};
+    result.survey = req.survey;
+    result.answers = responses;
+    res.status(201).send(gr(result, "Get Responses Success"));
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(gc("Server Error"));
+  }
+});
 
-router.post("/", (req, res, next) => {});
+router.post("/", async (req, res, next) => {
+  try {
+    req.body.surveyId = req.survey._id; // survey id 필드 추가
+    await Response.create(req.body);
+    res.status(201).send(gc("Create Response Success"));
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(gc("Server Error"));
+  }
+});
 
 module.exports = router;
