122dfd3c1de7e11a808cf113434afafcd3c5622c
SeungWookHan <hswook12@me.com>
2021-08-30 11:49:26 +0000

    Add user's survey get api

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,18 @@
 const express = require("express");
-const { getResponse: gr, getComment: gc } = require("../utils");
+const { getResponse: gr, getComment: gc } = require("../utils/response");
 const User = require("../models/user");
+const Survey = require("../models/survey");
 
 const router = express.Router();
+
+router.get("/surveys", async (req, res) => {
+  if (req.user) {
+    const surveys = await Survey.find({ userId: req.user.id }, { questions: 0 }).exec();
+    res.status(200).send(gr(surveys, "Send All Survey Succeess"));
+  } else {
+    res.status(400).send(gc("Not logged in."));
+  }
+});
 
 router.get("/", (req, res) => { });
 
