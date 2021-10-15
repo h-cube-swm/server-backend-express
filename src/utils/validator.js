fa1c08f3b896d8428ee6f83fd2d0d2ee8500fdb8
SeungWookHan <hswook12@me.com>
2021-10-15 16:39:08 +0000

    Add validator util function

---
+++ src/utils/validator.js
@@ -0,0 +1,14 @@
+// validation for express-validator
+const { validationResult } = require("express-validator");
+const { getResponse: gr } = require("../utils/response");
+
+exports.validatorErrorChecker = (req, res, next) => {
+  const errors = validationResult(req);
+  console.log(errors);
+  if (!errors.isEmpty()) {
+    return res
+      .status(400)
+      .send(gr({ errors: errors.array() }, "Invalid arguements"));
+  }
+  next();
+};
